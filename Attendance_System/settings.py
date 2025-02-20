b182b1de8a2e4fa8a0a8b95b77d6a626c00b53e4
ankitha <134725664+Ankitha-Velpula@users.noreply.github.com>
2025-02-20 23:04:37 +0530

    Update settings.py

--- Attendance_System/settings.py
+++ Attendance_System/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
 
 ALLOWED_HOSTS = ['*']
 
